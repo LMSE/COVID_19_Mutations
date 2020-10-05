691bd248295367e8d4f074486fcca7a5e68fb1ed
kiahdd <kianahaddadi@gmail.com>
2020-10-05 13:23:19 -0400

    resolve an error about file name

--- Code/setDirectories.m
+++ Code/setDirectories.m
@@ -15,17 +15,20 @@ dirc.Input = dirc.base + "Input";
 dirc.Output = dirc.base + "Output";
 dirc.Database = dirc.Input + "/Database";
 %% Setting up databases
-flag = 1; % the database exists
+flag = 1; % Reading frames exists
 if ~exist(dirc.Database, 'dir')
    mkdir(dirc.Database)
-   flag = 0; % the database does not exists
+   flag = 0; % Reading frames does not exists
 else
     if length({dir(dirc.Database).name})<= 2
         flage = 0; % the database folder is empty
     end
 end
-if dir(dirc.Output+"/Result_db*")
-    flag = 2; %% the output is already created
+file = dir(dirc.Output+"/Result_db*");
+exists = size(file);
+
+if exists(1)>0
+    flag = 2; %% Curated data is already available
     result =  dir(dirc.Output+"/Result_db*").name;
     array = strsplit(result,'_');
     m = strsplit(array{5},'.');
@@ -33,13 +36,11 @@ if dir(dirc.Output+"/Result_db*")
     n = str2double(array{4});
 end
 %% Setting up input fasta files
-fasta.db = {'/gisaid_hcov-19_2020-05-01.fasta';...
-    '/gisaid_hcov-19_2020-07-01.fasta';...
-    '/gisaid_hcov-19_2020-09-23.fasta'};
+fasta.db = {'/gisaid_hcov.fasta'};
 fasta.db = cellfun(@(data) dirc.Input+data,fasta.db,'uni',false);
 fasta.fastaseq = dirc.Input +"/Seq.fasta";
 fasta.pdb = dirc.Input +"/PDB.fasta";
-%% Break database to smaller unites
+%% Break reading frames to smaller unites to save time
 loc.a = dirc.Database+'Database_'+version.Input+'_FrameOne.mat'; %% database location for reading frame three
 loc.b = dirc.Database+'Database_'+version.Input+'_FrameTwo.mat'; %% database location for reading frame two
 loc.c = dirc.Database+'Database_'+version.Input+'_FrameThree.mat'; %% database location for reading frame one
@@ -48,7 +49,7 @@ loc.d = {dirc.Database+'Database_'+version.Input+'_NTSeq1.mat'; %% database loca
  dirc.Database+'Database_'+version.Input+'_NTSeq3.mat'}; %% database location for NT seq
 loc.e = dirc.Database+'Database_'+version.Input+'_Header.mat'; %% database location of Header
 %% clear variables
-clear n currentFolder;
+clear n currentFolder file exists;
 
 
 
