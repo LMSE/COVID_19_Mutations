7123f6a1d9ea40cab305c41027d780d23a2d176a
kiahdd <kianahaddadi@gmail.com>
2020-11-13 10:37:16 -0500

    debug

--- Code/DeleteNs.m
+++ Code/DeleteNs.m
@@ -5,6 +5,11 @@ if isfield(db_in,'NTAlignment')
     indx = find(cellfun(@(data) numel(unique(data{1}(3,:)))<=4,...
         db_in.NTAlignment));
     db_out = StructureCut(db_in,indx);
+	
+	indx = find(cellfun(@(data) numel(unique(data{1}(1,:)))<=4,...
+        db_out.NTAlignment));
+    db_out = StructureCut(db_out,indx);
+	
 elseif isfield(db_in,'Aalignment')
     indx = find(~cell2mat(db_in.Flag));
     db_out = StructureCut(db_in,indx);
